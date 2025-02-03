5ae40c692d8ee43056c2790ac5103965775de8fd
jones58 <108466184+jones58@users.noreply.github.com>
2025-02-03 12:18:55 +0000

    edit github username

--- src/resources.jsx
+++ src/resources.jsx
@@ -1,15 +1,22 @@
 const otherResources = [
   {
     name: "Creative Lives In Progress",
-    website: "https://www.creativelivesinprogress.com/opportunitiesboard",
+    website:
+      "https://www.creativelivesinprogress.com/opportunitiesboard",
   },
-  { name: "Find A Tech Job", website: "https://www.findatechjob.com/" },
+  {
+    name: "Find A Tech Job",
+    website: "https://www.findatechjob.com/",
+  },
   {
     name: "Silicon Milkround",
     website: "https://www.siliconmilkroundabout.com/",
   },
   { name: "Hackajob", website: "https://hackajob.com/talent" },
-  { name: "View my Github", website: "https://github.com/jones58" },
+  {
+    name: "View my Github",
+    website: "https://github.com/jackkershaw",
+  },
 ];
 
 function Resources() {
